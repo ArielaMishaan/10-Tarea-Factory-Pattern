da35c42f028c59b6c00b559daf95c8636528dba1
ArielaMishaan <109829847+ArielaMishaan@users.noreply.github.com>
2023-02-10 08:32:41 -0600

    cambio

--- Cocinero.java
+++ Cocinero.java
@@ -1,4 +1,36 @@
 public class Cocinero {
     
-    
+    public Plato getInstance(int tipo){
+        Plato plato = new Plato();
+
+        switch (tipo) {
+            case 1:
+                plato = new Hamburguesa();
+                break;
+
+            case 2:
+                plato = new Fideos();
+                break;
+
+            case 3:
+                plato = new Pizza();
+                break;
+
+            case 4:
+                plato = new Pastel();
+                break;
+
+            case 5:
+                plato = new Sandwich();
+                break;
+
+            case 6:
+                plato = new ArrozFrito();
+                break;
+        
+            default:
+                break;
+        }
+        return plato;
+    }
 }
