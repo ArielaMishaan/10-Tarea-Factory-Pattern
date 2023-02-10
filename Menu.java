95fc15120e04f65db88f4df40372c3c19fd66fda
CARPTatj <car22539@uvg.edu.gt>
2023-02-10 08:42:07 -0600

    Menu Clase

--- Menu.java
+++ Menu.java
@@ -1,4 +1,22 @@
 public class Menu {
-    private 
+    private Pizza pizza = new Pizza();
+    private Hamburguesa hamburguesa = new Hamburguesa();
+    private Fideos fideos = new Fideos();
+    private Pastel pastel = new Pastel();
+    private ArrozFrito arrozFrito = new ArrozFrito();
+    private Sandwich sandwich = new Sandwich();
+
+    public String imprimirMenu(){
+        String cadena = pizza.toString() + "/n" +
+        hamburguesa.toString() + "/n" +
+        fideos.toString() + "/n" + 
+        pastel.toString() + "/n" +
+        arrozFrito.toString() + "/n" +
+        sandwich.toString();
+        return cadena;
+    }
+
+
+
     
 }
